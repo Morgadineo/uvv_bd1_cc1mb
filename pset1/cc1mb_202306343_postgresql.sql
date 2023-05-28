f5c24120e85529f48d3e7dbe52809accbe054f44
Arthur Morgado Teixeira <106524323+Morgadineo@users.noreply.github.com>
2023-05-27 21:37:21 -0300

    Update cc1mb_202306343_postgresql.sql

--- pset1/cc1mb_202306343_postgresql.sql
+++ pset1/cc1mb_202306343_postgresql.sql
@@ -27,27 +27,27 @@ SET search_path TO lojas, '$user', public;
 
 -- Cria a tabela "produtos" e adiciona os comentários
 CREATE TABLE produtos (
-                produto_id 		  			NUMERIC(38) 	NOT NULL,
-                nome 			  			VARCHAR(255) 	NOT NULL,
+                produto_id 		  		NUMERIC(38) 	NOT NULL,
+                nome 			  		VARCHAR(255) 	NOT NULL,
                 preco_unitario 		 		NUMERIC(10,2),
-                detalhes 		  			BYTEA,
-                imagem 			  			BYTEA,
+                detalhes 		  		BYTEA,
+                imagem 			  		BYTEA,
                 imagem_mime_type 	  		VARCHAR(512),
                 imagem_arquivo 		  		VARCHAR(512),
                 imagem_charset 		  		VARCHAR(512),
-                imagem_ultima_atualizacao 	DATE,
+                imagem_ultima_atualizacao 		DATE,
                 CONSTRAINT produtos_pk PRIMARY KEY (produto_id)
 );
 
-COMMENT ON TABLE produtos 								IS 'Tabela de produtos.';
-COMMENT ON COLUMN produtos.produto_id 					IS 'Id do produto. (PK da tabela produtos).';
-COMMENT ON COLUMN produtos.nome 						IS 'Nome do produto.';
-COMMENT ON COLUMN produtos.preco_unitario 				IS 'Preço unitário do produto.';
-COMMENT ON COLUMN produtos.detalhes 					IS 'Detalhes do produto.';
-COMMENT ON COLUMN produtos.imagem 						IS 'Imagem do produto.';
-COMMENT ON COLUMN produtos.imagem_mime_type 			IS 'Tipo do mime da imagem do produto.';
-COMMENT ON COLUMN produtos.imagem_arquivo 				IS 'Arquivo da imagem do produto.';
-COMMENT ON COLUMN produtos.imagem_charset 				IS 'Charset da imagme do produto.';
+COMMENT ON TABLE produtos 				IS 'Tabela de produtos.';
+COMMENT ON COLUMN produtos.produto_id 			IS 'Id do produto. (PK da tabela produtos).';
+COMMENT ON COLUMN produtos.nome 			IS 'Nome do produto.';
+COMMENT ON COLUMN produtos.preco_unitario 		IS 'Preço unitário do produto.';
+COMMENT ON COLUMN produtos.detalhes 			IS 'Detalhes do produto.';
+COMMENT ON COLUMN produtos.imagem 			IS 'Imagem do produto.';
+COMMENT ON COLUMN produtos.imagem_mime_type 		IS 'Tipo do mime da imagem do produto.';
+COMMENT ON COLUMN produtos.imagem_arquivo 		IS 'Arquivo da imagem do produto.';
+COMMENT ON COLUMN produtos.imagem_charset 		IS 'Charset da imagme do produto.';
 COMMENT ON COLUMN produtos.imagem_ultima_atualizacao 	IS 'Data da ultima atualização da imagem do produto.';
 
 -- Constraints da tabela produtos
@@ -57,31 +57,31 @@ ALTER TABLE produtos ADD CONSTRAINT chk_preco_unitario CHECK (preco_unitario >= 
 
 -- Cria a tabela "lojas" e adiciona os comentários
 CREATE TABLE lojas (
-                loja_id 					NUMERIC(38)  NOT NULL,
-                nome 						VARCHAR(255) NOT NULL,
+                loja_id 				NUMERIC(38)  NOT NULL,
+                nome 					VARCHAR(255) NOT NULL,
                 endereco_web 				VARCHAR(100),
                 endereco_fisico 			VARCHAR(512),
-                latitude 					NUMERIC,
-                longitude 					NUMERIC,
-                logo 						BYTEA,
+                latitude 				NUMERIC,
+                longitude 				NUMERIC,
+                logo 					BYTEA,
                 logo_mime_type 				VARCHAR(512),
                 logo_arquivo 				VARCHAR(512),
                 logo_charset 				VARCHAR(512),
-                logo_ultima_atualizacao 	DATE,
+                logo_ultima_atualizacao 		DATE,
                 CONSTRAINT loja_id PRIMARY KEY (loja_id)
 );
 
-COMMENT ON TABLE lojas 								IS 'Tabela de lojas.';
-COMMENT ON COLUMN lojas.loja_id 					IS 'Id da loja. (PK da tabela lojas)';
-COMMENT ON COLUMN lojas.nome 						IS 'Nome da loja.';
-COMMENT ON COLUMN lojas.endereco_web 				IS 'Endereço web da loja.';
-COMMENT ON COLUMN lojas.endereco_fisico 			IS 'Endereço físico da loja.';
-COMMENT ON COLUMN lojas.latitude 					IS 'Latitude da loja.';
-COMMENT ON COLUMN lojas.longitude 					IS 'Longitude da loja.';
-COMMENT ON COLUMN lojas.logo 						IS 'Logo da loja.';
-COMMENT ON COLUMN lojas.logo_mime_type 				IS 'Tipo da logo mime da loja.';
-COMMENT ON COLUMN lojas.logo_arquivo 				IS 'Arquivo da logo da loja.';
-COMMENT ON COLUMN lojas.logo_charset 				IS 'Logo charset da loja.';
+COMMENT ON TABLE lojas 					IS 'Tabela de lojas.';
+COMMENT ON COLUMN lojas.loja_id 			IS 'Id da loja. (PK da tabela lojas)';
+COMMENT ON COLUMN lojas.nome 				IS 'Nome da loja.';
+COMMENT ON COLUMN lojas.endereco_web 			IS 'Endereço web da loja.';
+COMMENT ON COLUMN lojas.endereco_fisico 		IS 'Endereço físico da loja.';
+COMMENT ON COLUMN lojas.latitude 			IS 'Latitude da loja.';
+COMMENT ON COLUMN lojas.longitude 			IS 'Longitude da loja.';
+COMMENT ON COLUMN lojas.logo 				IS 'Logo da loja.';
+COMMENT ON COLUMN lojas.logo_mime_type 			IS 'Tipo da logo mime da loja.';
+COMMENT ON COLUMN lojas.logo_arquivo 			IS 'Arquivo da logo da loja.';
+COMMENT ON COLUMN lojas.logo_charset 			IS 'Logo charset da loja.';
 COMMENT ON COLUMN lojas.logo_ultima_atualizacao 	IS 'Data de ultima atualização da logo da loja.';
 
 -- Constraints da tabela lojas
@@ -91,18 +91,18 @@ ALTER TABLE lojas ADD CONSTRAINT chk_enderecos 	CHECK(endereco_web IS not null O
 
 -- Cria a tabela "estoques" e adiciona os comentários
 CREATE TABLE estoques (
-                estoque_id 				NUMERIC(38) NOT NULL,
-                loja_id 				NUMERIC(38) NOT NULL,
-                produto_id 				NUMERIC(38) NOT NULL,
-                quantidade 				NUMERIC(38) NOT NULL,
+                estoque_id 		NUMERIC(38) NOT NULL,
+                loja_id 		NUMERIC(38) NOT NULL,
+                produto_id 		NUMERIC(38) NOT NULL,
+                quantidade 		NUMERIC(38) NOT NULL,
                 CONSTRAINT estoques_pk PRIMARY KEY (estoque_id)
 );
 
 COMMENT ON TABLE estoques 		IS 'Tabela de estoques.';
-COMMENT ON COLUMN estoques.estoque_id IS 'Id do estoque. (PK da tabela estoques).';
+COMMENT ON COLUMN estoques.estoque_id 	IS 'Id do estoque. (PK da tabela estoques).';
 COMMENT ON COLUMN estoques.loja_id 	IS 'Id da loja que realizou o estoque. (FK da tabela lojas)';
-COMMENT ON COLUMN estoques.produto_id IS 'Id do produto em estoque. (FK da tabela produtos).';
-COMMENT ON COLUMN estoques.quantidade IS 'Quantidade do produto estocado.';
+COMMENT ON COLUMN estoques.produto_id 	IS 'Id do produto em estoque. (FK da tabela produtos).';
+COMMENT ON COLUMN estoques.quantidade 	IS 'Quantidade do produto estocado.';
 
 -- Constraints da tabela estoques
 ALTER TABLE estoques ADD CONSTRAINT chk_estoque_id CHECK (estoque_id >= 0);
@@ -120,7 +120,7 @@ CREATE TABLE clientes (
                 CONSTRAINT cliente_id PRIMARY KEY (client_id)
 );
 
-COMMENT ON TABLE clientes 	       		IS 'Tabela com as nformações dos clientes.';
+COMMENT ON TABLE clientes 	       	IS 'Tabela com as nformações dos clientes.';
 COMMENT ON COLUMN clientes.client_id 	IS 'Id dos clientes. (PK da tabela clientes)';
 COMMENT ON COLUMN clientes.email     	IS 'Email do cliente.';
 COMMENT ON COLUMN clientes.nome      	IS 'Nome do cliente.';
@@ -137,7 +137,7 @@ CREATE TABLE envios (
                 envio_id 	 		NUMERIC(38)  NOT NULL,
                 loja_id 	 		NUMERIC(38)  NOT NULL,
                 client_id 	 		NUMERIC(38)  NOT NULL,
-                endereco_entrega 	VARCHAR(512) NOT NULL,
+                endereco_entrega 		VARCHAR(512) NOT NULL,
                 status 		 		VARCHAR(15)  NOT NULL,
                 CONSTRAINT envios_pk PRIMARY KEY (envio_id)
 );
@@ -146,7 +146,7 @@ COMMENT ON TABLE envios 	  	    IS 'Tabela de envios.';
 COMMENT ON COLUMN envios.envio_id 	    IS 'Id do envio. (PK da tabela envios)';
 COMMENT ON COLUMN envios.loja_id 	    IS 'Id da loja que realizou o envio. (FK da tabela lojas)';
 COMMENT ON COLUMN envios.client_id 	    IS 'Id do cliente que realizou o envio. (FK da tabela clientes)';
-COMMENT ON COLUMN envios.endereco_entrega IS 'Endereço de entrega do envio.';
+COMMENT ON COLUMN envios.endereco_entrega   IS 'Endereço de entrega do envio.';
 COMMENT ON COLUMN envios.status 	    IS 'Status do envio.';
 
 -- Constraints da tabela envios
@@ -162,7 +162,7 @@ CREATE TABLE pedidos (
                 CONSTRAINT pedidos_pk PRIMARY KEY (pedido_id)
 );
 
-COMMENT ON TABLE pedidos 				IS 'Tabela de pedidos.';
+COMMENT ON TABLE pedidos 		IS 'Tabela de pedidos.';
 COMMENT ON COLUMN pedidos.pedido_id   	IS 'Id do pedido. (PK da tabela pedidos)';
 COMMENT ON COLUMN pedidos.data_hora   	IS 'Data e hora e do pedido.';
 COMMENT ON COLUMN pedidos.client_id   	IS 'Id do cliente que realizou o pedido. (FK da tabela clientes)';
@@ -174,21 +174,21 @@ ALTER TABLE pedidos ADD CONSTRAINT chk_status CHECK (status IN ('CANCELADO', 'CO
 
 -- Cria a tabela "itens" e adiciona os comentários
 CREATE TABLE pedidos_itens (
-                pedido_id 			NUMERIC(38)   NOT NULL,
-                produto_id 			NUMERIC(38)   NOT NULL,
+                pedido_id 		NUMERIC(38)   NOT NULL,
+                produto_id 		NUMERIC(38)   NOT NULL,
                 numero_da_linha 	NUMERIC(38)   NOT NULL,
                 preco_unitario  	NUMERIC(10,2) NOT NULL,
                 quantidade  		NUMERIC(38)   NOT NULL,
-                envio_id 			NUMERIC(38)   NOT NULL,
+                envio_id 		NUMERIC(38)   NOT NULL,
                 CONSTRAINT pedidos_itens_pk PRIMARY KEY (pedido_id, produto_id)
 );
 
-COMMENT ON TABLE pedidos_itens 				  	IS 'Tabela de itens pedidos.';
-COMMENT ON COLUMN pedidos_itens.pedido_id 	  	IS 'Id do item pedido. (PK da tabela pedidos)';
+COMMENT ON TABLE pedidos_itens 			IS 'Tabela de itens pedidos.';
+COMMENT ON COLUMN pedidos_itens.pedido_id 	IS 'Id do item pedido. (PK da tabela pedidos)';
 COMMENT ON COLUMN pedidos_itens.produto_id      IS 'Id do produto pedido. (PK da tabela produtos).';
 COMMENT ON COLUMN pedidos_itens.numero_da_linha IS 'Numero da linha do item pedido.';
 COMMENT ON COLUMN pedidos_itens.preco_unitario  IS 'Preco unitário do produto pedido.';
-COMMENT ON COLUMN pedidos_itens.quantidade 	  	IS 'Quantidade do item pedido,';
+COMMENT ON COLUMN pedidos_itens.quantidade 	IS 'Quantidade do item pedido,';
 COMMENT ON COLUMN pedidos_itens.envio_id        IS 'Id do envio. (PK da tabela envios)';
 
 -- Constraints da tabela pedidos_itens
